64166c17bb658fd318682b82d46768eb77417b6c
Unknown <0104kumakiri@gmail.com>
2018-05-02 18:11:56 +0900

    最初のコミット

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        Debug.Log(sum);
+    }
+}
